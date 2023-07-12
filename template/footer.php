167f4ff2b9983d0c117eebaffa676a8b464dd931
TheBui2308 <thebui2308@gmail.com>
2023-07-12 14:36:09 +0700

    first commit

---
+++ template/footer.php
@@ -0,0 +1,40 @@
+<script>
+    let slideIndex = 1;
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        let dots = document.getElementsByClassName("dot");
+        if (n > slides.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = slides.length }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+    }
+</script>
+
+<footer>
+    <section class="pt-[200px] max-w-7xl mx-auto">
+        <div class="blue bg-green-100 py-6 text-center font-bold text-2xl">
+            <h1>Bùi Quang Thể PH28513</h1>
+        </div>
+    </section>
+</footer>
+
+</body>
+
+</html>
